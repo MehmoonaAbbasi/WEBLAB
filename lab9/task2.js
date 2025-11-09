80aab7cb03ab2ab9b87722c0d42af96f89ce8a34
MehmoonaAbbasi <mehmoonaabbasi651@gmail.com>
2025-11-09 21:39:47 +0500

    Add files via upload

---
+++ lab9/task2.js
@@ -0,0 +1,2 @@
+const paragraph = document.getElementById('myPara');
+paragraph.innerHTML = 'This is the <b>updated</b> paragraph content!';
---
+++ lab9/task3.js
@@ -0,0 +1,2 @@
+const paragraph = document.getElementById('myPara');
+paragraph.textContent = 'Islamic Republic of Pakistan, is a country in South Asia.!';
